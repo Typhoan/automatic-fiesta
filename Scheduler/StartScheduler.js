0f6f07d1c21fc96425cc8d5d11464079b07f3045
Ian McPherson <Tidus1101@gmail.com>
2022-04-04 23:45:03 -0400

    fix

--- Scheduler/StartScheduler.js
+++ Scheduler/StartScheduler.js
@@ -5,14 +5,15 @@ import { schedule } from "/Scheduler/Scheduler.js";
 
 /** @param {NS} ns **/
 export async function main(ns) {
-    let ownedServers = getOwnedServers(ns);
+
+    let ownedServers = getOwnedServers(ns)
 
     let hackedServers = ownedServers.filter(server => server.moneyMax > 0);
     let agentServers = ownedServers.filter(server => server.hostname.includes("HackBot") || server.hostname.includes("Home")).sort((a,b) => b.maxRam - a.maxRam);
 
     await makeHackedServersPristine(ns, hackedServers, agentServers);
-    // find the most profitable hacked servers, and find agent server with most available ram. 
 
+    // find the most profitable hacked servers, and find agent server with most available ram. 
     let mostProfitableServers = await getMostProfitableServers(ns, hackedServers);
 
     // now that servers are pristine and ordered use a scheduler to schedule scripts to be exectuted on them.
@@ -27,7 +28,7 @@ export async function main(ns) {
     while(true){
 
         for (let i = 0; i < numberServersToHack; i++) {
-            offsets[i] = await schedule(ns, mostProfitableServers[i], agentServers[i], LastScheduledTime[i]);
+            LastScheduledTime[i] = await schedule(ns, mostProfitableServers[i], agentServers[i], LastScheduledTime[i]);
         }
 
         ns.sleep(4000);
